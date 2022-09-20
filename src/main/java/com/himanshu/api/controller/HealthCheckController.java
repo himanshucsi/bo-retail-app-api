4360263b1d7b307acd89d4826c488eeac550c311
himanshucsi <himanshucsi.suman@gmail.com>
2022-09-20 12:28:43 +0530

    Added Project to github

---
+++ src/main/java/com/himanshu/api/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.himanshu.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("checkHealth")
+	public String testAppAndDBStatus() {
+		return "Running successfully";
+	}
+	
+}
