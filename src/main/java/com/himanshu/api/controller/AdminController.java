8068dd10c9296ae9fc368095cf4eb7bc0b6719ed
himanshucsi <himanshucsi.suman@gmail.com>
2022-09-22 22:20:05 +0530

    Added Application Logs with Application Insights

--- src/main/java/com/himanshu/api/controller/AdminController.java
+++ src/main/java/com/himanshu/api/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.himanshu.api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +17,25 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class AdminController {
-	
+
 	@Autowired
 	private ResponseBuilder responseBuilder;
-	
+
 	@Autowired
 	private AdminService adminService;
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
+
 	@PostMapping("admin")
 	public ResponseEntity<ResponseDto> addAdmin(@RequestBody Admin admin) {
-		
+
+		LOGGER.info("Entered into Admin Cotroller {}", admin.getAdminId());
 		ResponseDto responseDto = null;
-		
-		if(null != adminService.addAdmin(admin)) {
+
+		if (null != adminService.addAdmin(admin)) {
 			responseDto = responseBuilder.prepareAddOrUpdateResponse();
 		}
-		
+		LOGGER.info("Exiting From Admin Cotroller {}", admin.getAdminId());
 		return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.CREATED);
 	}
 
--- src/main/java/com/himanshu/api/controller/HealthCheckController.java
+++ src/main/java/com/himanshu/api/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package com.himanshu.api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +12,25 @@ import com.himanshu.api.service.HealthCheckService;
 
 @RestController
 public class HealthCheckController {
-	
+
 	@Autowired
 	private HealthCheckService healthCheckService;
-	
+
 	@Autowired
 	private ResponseBuilder responseBuilder;
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
+
 	@GetMapping("checkHealth")
 	public ResponseDto testAppAndDBStatus() {
-		
+
+		LOGGER.info("Entered into HealthCheck Cotroller");
 		healthCheckService.checkHealth();
-		
+
 		ResponseDto responseDto = responseBuilder.prepareHealthCheckResponse("Connected");
-		
-		
+
+		LOGGER.info("Exiting From HealthCheck Cotroller");
 		return responseDto;
 	}
-	
+
 }
